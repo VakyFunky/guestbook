deb088c7df0ef60909edb835006c69de8e315541
Vanja Cvetković <cve.vanja@gmail.com>
2022-06-13 00:05:38 +0200

    error page

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { LeftOutlined } from '@ant-design/icons';
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='content'>
+      <h1 style={{textAlign:'center'}}>oups! </h1>
+      <h3 style={{textAlign:'center'}}>
+      <Link to="/" className="btn"> <LeftOutlined />back to Guestbook</Link>
+      </h3>
+      
+    </div>
   )
 }
 
